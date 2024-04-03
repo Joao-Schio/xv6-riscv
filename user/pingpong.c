edecf16f1664690c2ca1901140ef414d95bf7654
Joao-Schio <jp@santoandrea.com.br>
2024-04-03 10:21:24 -0400

    nao tava funcionando antes mas ai comecou a funcionar > ??????? <

--- user/pingpong.c
+++ user/pingpong.c
@@ -20,17 +20,17 @@ int main(void){
 
     if(PID > 0){
         close(pai[0]);
-        write(pai[1],"ping",5); // 5 bytes por causa do \0 ??
+        write(pai[1],"ping",4); // 5 bytes por causa do \0 ??
         close(filho[1]);
-        read(filho[0],buffer,5);
+        read(filho[0],buffer,4);
         printf("%d recebeu %s\n",getpid(),buffer);
     }
     else if(PID == 0){
         close(pai[1]);
-        read(pai[0],buffer,5);
+        read(pai[0],buffer,4);
         close(filho[0]);
         printf("%d recebeu %s\n",getpid(),buffer);
-        write(filho[1],"pong",5);
+        write(filho[1],"pong",4);
     }
 
     else{
